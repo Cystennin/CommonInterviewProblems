b20798b9a33c075ed76224812a2ebdce9888829f
Ken Stoner <stonerk@amazon.com>
2016-03-10 09:50:25 -0800

    Adds failing tests for DoublyLinkedList_NodeInterfaceExtensions.ToDoublyLinkedList

---
+++ CommonInterviewProblems/DataStructures/Graphs/LinkedLists/DoublyLinkedList_NodeInterfaceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.Personal.CommonInterviewProblems.DataStructures.Graphs.LinkedLists
+{
+    public static class DoublyLinkedList_NodeInterfaceExtensions
+    {
+    }
+}
