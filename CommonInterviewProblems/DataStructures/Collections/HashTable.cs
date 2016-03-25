af283c71a28e1ddc2fc23679c4963f18870beb8b
Ken Stoner <stonerk@amazon.com>
2016-03-25 15:14:24 -0700

    Adds implementation for Hashtable

---
+++ CommonInterviewProblems/DataStructures/Collections/HashTable.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raven.Personal.CommonInterviewProblems.DataStructures.Collections
+{
+    public class RavenHashTable<T>
+    {
+        private static readonly int INITIAL_SIZE = 10;
+        private LinkedList<T>[] buckets;
+        private int size;
+
+        public RavenHashTable()
+        {
+            this.buckets = new LinkedList<T>[INITIAL_SIZE];
+        }
+
+        public int Size
+        {
+            get
+            {
+                return this.size;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return this.buckets.Length;
+            }
+        }
+
+        public RavenHashTable<T> Add(T element)
+        {
+            validate(element);
+            if (resizeNeeded())
+            {
+                resize();
+            }
+            int hash = element.GetHashCode();
+            int bucketIndex = Math.Abs(hash % buckets.Length);
+            LinkedList<T> bucket = buckets[bucketIndex];
+            if (null == bucket)
+            {
+                bucket = new LinkedList<T>();
+                buckets[bucketIndex] = bucket;
+            }
+            bucket.AddLast(element);
+            this.size++;
+            return this;
+        }
+
+        public bool Exists(T element)
+        {
+            int hash = element.GetHashCode();
+            int bucketIndex = hash % buckets.Length;
+            LinkedList<T> bucket = buckets[bucketIndex];
+            if (null == bucket)
+            {
+                return false;
+            }
+            if (bucket.Find(element) != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public T[] ToArray()
+        {
+            T[] array = new T[this.size];
+            foreach (LinkedList<T> bucket in buckets)
+            {
+                if (null == bucket)
+                {
+                    continue;
+                }
+                int arrayIndex = 0;
+                foreach (T element in bucket)
+                {
+                    array[arrayIndex++] = element;
+                }
+            }
+            return array;
+        }
+
+        private void resize()
+        {
+            LinkedList<T>[] newBucketList = new LinkedList<T>[this.size * 2];
+            foreach (LinkedList<T> bucket in buckets)
+            {
+                if (null == bucket)
+                {
+                    continue;
+                }
+                foreach(T element in bucket)
+                {
+                    int hash = element.GetHashCode();
+                    int bucketIndex = Math.Abs(hash % newBucketList.Length);
+                    LinkedList<T> newBucket = newBucketList[bucketIndex];
+                    if (null == newBucket)
+                    {
+                        newBucket = new LinkedList<T>();
+                        newBucketList[bucketIndex] = newBucket;
+                    }
+                    newBucket.AddLast(element);
+                }
+            }
+            this.buckets = newBucketList;
+        }
+
+        private bool resizeNeeded()
+        {
+            float loadFactor = ((float)this.size / (float)buckets.Length);
+            if (loadFactor >.75)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void validate(T element)
+        {
+            if (null == element)
+            {
+                throw new ArgumentNullException(nameof(element), "Element canot be null!");
+            }
+        }
+    }
+}
---
+++ CommonInterviewProblemsTests/DataStructures/Collections/HashTableTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Raven.Personal.CommonInterviewProblems.DataStructures.Collections;
+
+namespace Raven.Personal.CommonInterviewProblems.Tests.DataStructures.Collections
+{
+    [TestClass]
+    public class HashTableTests
+    {
+        [TestMethod]
+        public void Resizes_When_Load_Over_75()
+        {
+            RavenHashTable<string> hashTable = new RavenHashTable<string>();
+            hashTable.Add("1")
+                .Add("2")
+                .Add("3")
+                .Add("4")
+                .Add("5")
+                .Add("6")
+                .Add("7")
+                .Add("8")
+                .Add("9");
+            Assert.AreEqual(9, hashTable.Size);
+            Assert.IsTrue(hashTable.Capacity > 10);
+        }
+
+        [TestMethod]
+        public void Can_Add_Identical_Value_After_Resize()
+        {
+            RavenHashTable<string> hashTable = new RavenHashTable<string>();
+            hashTable.Add("AAA")
+                .Add("BBB")
+                .Add("CCC")
+                .Add("DDD")
+                .Add("EEE")
+                .Add("FFF")
+                .Add("GGG")
+                .Add("HHH")
+                .Add("999");
+            hashTable.Add("ZZZ");
+            Assert.AreEqual(10, hashTable.Size);
+            Assert.IsTrue(hashTable.Capacity > 10);
+        }
+
+        [TestMethod]
+        public void Can_FindValue()
+        {
+            RavenHashTable<string> hashTable = new RavenHashTable<string>();
+            hashTable.Add("AAA")
+                .Add("BBB")
+                .Add("CCC")
+                .Add("DDD")
+                .Add("EEE")
+                .Add("FFF")
+                .Add("GGG")
+                .Add("HHH")
+                .Add("999");
+            hashTable.Add("ZZZ");
+            Assert.AreEqual(true, hashTable.Exists("GGG"));
+        }
+    }
+}
